4e029745f1509d47dba266d20fe0949902658238
aburuvan <93507543+aburuvan2908@users.noreply.github.com>
2021-11-16 14:03:32 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,15 @@
+package nextgen;
+
+import nineties.Phone;
+
+public class Mobile extends Phone
+{
+public static void main(String[] args)
+{
+ Mobile vivo = new Mobile();
+ vivo.call();
+ vivo.receive();
+
+
+}
+}
---
+++ Phone.java
@@ -0,0 +1,25 @@
+package nineties;
+public class Phone
+{
+public static void main(String[] args)
+{
+
+
+
+}
+public void call()
+{
+  System.out.println("out going");
+  }
+  public void receive()
+  {
+  
+  System.out.println("incoming ");
+  }
+  
+  }
+
+  
+
+
+
