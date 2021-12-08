b6bbe215f344c4232f83ad04eb565de40a84f0e9
aburuvan <93507543+aburuvan2908@users.noreply.github.com>
2021-12-08 18:52:18 +0530

    WHILE LOOP FACTORIAL

---
+++ Factorialdemo.java
@@ -0,0 +1,18 @@
+package tamilnadu.erode;
+
+public class Factorialdemo {
+
+	public static void main(String[] args) {
+		int no = 1, x = 1; 
+		while (x<=5) {
+			no=(no * x);
+			//System.out.println("print no" + no);
+			
+			 
+			 x=x+1;
+		}
+		System.out.println("print" + no);
+
+	}
+
+}
