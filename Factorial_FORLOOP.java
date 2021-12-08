22ce234e7b4dbb909d9052cf09c3d8d45a4c9261
aburuvan <93507543+aburuvan2908@users.noreply.github.com>
2021-12-08 18:51:32 +0530

    for loop  factorial

---
+++ Factorial_FORLOOP.java
@@ -0,0 +1,14 @@
+package tamilnadu.erode;
+
+public class Factorial_FORLOOP {
+	public static void main(String[] args) {
+		int i,value=1;
+		for (i=1;i<=5;i++)
+		{
+			value=value*i;
+			//System.out.println(value);
+		}
+	System.out.println(value);
+	}
+
+}
