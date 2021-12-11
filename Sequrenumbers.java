0c84dc67c1cd30377a0745613b644aa140458f9d
aburuvan <93507543+aburuvan2908@users.noreply.github.com>
2021-12-11 14:00:42 +0530

    SEQURENUMBERS

---
+++ Sequrenumbers.java
@@ -0,0 +1,17 @@
+package tamilnadu.erode;
+
+public class Sequrenumbers {
+
+	public static void main(String[] args) {
+		int i =0; int no = 0;
+		while(i<=5)
+		{
+			no=( i*i);
+			//i++;
+			System.out.println(+no);
+			i++;
+		}
+
+	}
+
+}
